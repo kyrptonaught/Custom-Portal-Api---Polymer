7c5db7bc9af87f23fe12019c232c0315b44fe546
kyrptonaught <anthonybuttillo10@gmail.com>
2022-05-11 16:22:34 -0400

    Fix nether portals not having the correct axis.
    Also adds support back to clients with a version of CPA installed.

--- src/main/java/net/kyrptonaught/servercustomportals/mixin/VirtualPortalBlock.java
+++ src/main/java/net/kyrptonaught/servercustomportals/mixin/VirtualPortalBlock.java
@@ -2,9 +2,15 @@ package net.kyrptonaught.servercustomportals.mixin;
 
 import eu.pb4.polymer.api.block.PolymerBlock;
 import net.kyrptonaught.customportalapi.CustomPortalBlock;
+import net.kyrptonaught.customportalapi.networking.ForcePlacePortalPacket;
+import net.kyrptonaught.customportalapi.util.CustomPortalHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(CustomPortalBlock.class)
@@ -13,5 +19,16 @@ public class VirtualPortalBlock implements PolymerBlock {
     @Override
     public Block getPolymerBlock(BlockState state) {
         return Blocks.NETHER_PORTAL;
+    }
+
+    @Override
+    public BlockState getPolymerBlockState(BlockState state) {
+        Direction.Axis dir = CustomPortalHelper.getAxisFrom(state);
+        return this.getPolymerBlock(state).getDefaultState().with(NetherPortalBlock.AXIS, dir);
+    }
+
+    @Override
+    public void onPolymerBlockSend(ServerPlayerEntity player, BlockPos.Mutable pos, BlockState blockState) {
+        ForcePlacePortalPacket.sendForcePacket(player, pos);
     }
 }
